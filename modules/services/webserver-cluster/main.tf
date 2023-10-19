87ae15cfbc0908ac45efd5fe43f752a8930445ea
Dima <sushkevichd@gmail.com>
2023-10-19 20:00:57 +0300

    replace inline blocks

--- modules/services/webserver-cluster/main.tf
+++ modules/services/webserver-cluster/main.tf
@@ -1,12 +1,14 @@
 resource "aws_security_group" "instance" {
   name = "${var.cluster_name}-instance"
+}
 
-  ingress {
-    from_port = var.server_port
-    to_port = var.server_port
-    protocol = local.tcp_protocol
-    cidr_blocks = [local.all_ips]
-  }
+resource "aws_security_group_rule" "allow_server_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.instance.id
+  from_port = var.server_port
+  to_port = var.server_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_launch_configuration" "example" {
@@ -78,22 +80,24 @@ resource "aws_lb_listener" "example" {
 
 resource "aws_security_group" "alb" {
   name = "${var.cluster_name}-alb"
+}
 
-  # Allow inbound HTTP requests
-  ingress {
-    from_port = local.http_port
-    to_port = local.http_port
-    protocol = local.tcp_protocol
-    cidr_blocks = [local.all_ips]
-  }
+resource "aws_security_group_rule" "allow_http_inbound" {
+  type = "ingress"
+  security_group_id = aws_security_group.alb.id
+  from_port = local.http_port
+  to_port = local.http_port
+  protocol = local.tcp_protocol
+  cidr_blocks = local.all_ips
+}
 
-  # Allow all outbound requests
-  egress {
-    from_port = local.any_port
-    to_port = local.any_port
-    protocol = local.any_protocol
-    cidr_blocks = [local.all_ips]
-  }
+resource "aws_security_group_rule" "allow_all_outbound" {
+  type = "egress"
+  security_group_id = aws_security_group.alb.id
+  from_port = local.any_port
+  to_port = local.any_port
+  protocol = local.any_protocol
+  cidr_blocks = local.all_ips
 }
 
 resource "aws_lb_target_group" "asg" {
